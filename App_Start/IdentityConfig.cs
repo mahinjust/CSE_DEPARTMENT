967de0d8f7654fb29540e26e003f7b758dec2cac
Ashav Noman Mahin <mahin.just@hotmail.com>
2021-01-03 14:07:04 +0600

    3/1/21 ups

--- App_Start/IdentityConfig.cs
+++ App_Start/IdentityConfig.cs
@@ -40,7 +40,7 @@ namespace CSE_DEPARTMENT
         {
             #region formatter
             string text = string.Format("Please click on this link to {0}: {1}", message.Subject, message.Body);
-            string html = "Please confirm your account by clicking this link: <a href=\"" + message.Body + "\">link</a><br/>";
+            string html = "Please confirm your account by clicking this link: <a href=\"" + message.Body + "\"></a><br/>";
 
             html += HttpUtility.HtmlEncode(@"Or click on the copy the following link on the browser:" + message.Body);
             #endregion
