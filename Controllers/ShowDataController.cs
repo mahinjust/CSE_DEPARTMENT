8c6ece29c3dc3d6be6a971a566cb78c9e5b34fa6
Ashav Noman Mahin <mahin.just@hotmail.com>
2021-01-17 01:36:55 +0600

    17-1-21(Update)

--- Controllers/ShowDataController.cs
+++ Controllers/ShowDataController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using CSE_DEPARTMENT.Models;
 using CSE_DEPARTMENT.ViewModel;
-
+using System.Linq.Dynamic;
 
 namespace CSE_DEPARTMENT.Controllers
 {
@@ -13,34 +13,32 @@ namespace CSE_DEPARTMENT.Controllers
     {
         CSE_DEPARTMENT_DBEntities obj = new CSE_DEPARTMENT_DBEntities();
         // GET: ShowData
-        public ActionResult Multidata()
+        public ActionResult Multidata(int page = 1, string sort = "Name", string sortdir = "asc", string search = "")
         {
 
-            var mymodel = new Multipledata();
-
-            mymodel.AspNetRole = obj.AspNetRoles.ToList();
-            return View(mymodel);
+            int pageSize = 10;
+            int totalRecord = 0;
+            if (page < 1) page = 1;
+            int skip = (page * pageSize) - pageSize;
+            var data = GetRoles(search, sort, sortdir, skip, pageSize, out totalRecord);
+            ViewBag.TotalRows = totalRecord;
+            ViewBag.search = search;
+            return View(data);
         }
 
-        public ActionResult Multidata2()
-        {
-            CSE_DEPARTMENT_DBEntities obj = new CSE_DEPARTMENT_DBEntities();
-            //var model = obj.AspNetUsers.ToList();
-            //return View(model);
-            return View(obj.SearchUsers("").ToList());
-
-
-        }
-
-        [HttpPost]
-        public ActionResult Multidata2(string Email)
+        public ActionResult Multidata2(int page = 1, string sort = "Email", string sortdir = "asc", string search = "")
         {
 
-            CSE_DEPARTMENT_DBEntities obj = new CSE_DEPARTMENT_DBEntities();
-            return View(obj.SearchUsers(Email).ToList());
+            int pageSize = 10;
+            int totalRecord = 0;
+            if (page < 1) page = 1;
+            int skip = (page * pageSize) - pageSize;
+            var data = GetUsers(search, sort, sortdir, skip, pageSize, out totalRecord);
+            ViewBag.TotalRows = totalRecord;
+            ViewBag.search = search;
+            return View(data);
 
         }
-
 
 
 
@@ -50,6 +48,51 @@ namespace CSE_DEPARTMENT.Controllers
             obj.AspNetUsers.Remove(model);
             obj.SaveChanges();
             return RedirectToAction("Multidata2", "ShowData");
+        }
+
+
+        public List<AspNetUser> GetUsers(string search, string sort, string sortdir, int skip, int pageSize, out int totalRecord)
+        {
+            using (CSE_DEPARTMENT_DBEntities obj = new CSE_DEPARTMENT_DBEntities())
+            {
+                var v = (from a in obj.AspNetUsers
+                         where
+                                 a.Id.Contains(search) ||
+                                 a.Email.Contains(search) ||
+                                 a.PhoneNumber.Contains(search)
+
+                         select a
+                                );
+                totalRecord = v.Count();
+                v = v.OrderBy(sort + " " + sortdir);
+                if (pageSize > 0)
+                {
+                    v = v.Skip(skip).Take(pageSize);
+                }
+                return v.ToList();
+            }
+        }
+
+        public List<AspNetRole> GetRoles(string search, string sort, string sortdir, int skip, int pageSize, out int totalRecord)
+        {
+            using (CSE_DEPARTMENT_DBEntities obj = new CSE_DEPARTMENT_DBEntities())
+            {
+                var v = (from a in obj.AspNetRoles
+                         where
+                                 a.Id.Contains(search) ||
+                                 a.Name.Contains(search)
+
+
+                         select a
+                                );
+                totalRecord = v.Count();
+                v = v.OrderBy(sort + " " + sortdir);
+                if (pageSize > 0)
+                {
+                    v = v.Skip(skip).Take(pageSize);
+                }
+                return v.ToList();
+            }
         }
 
 
