95c188559da060fc9db43e29cb05f6b08635be11
Md. Ashav Noman Mahin <56822729+mahinjust@users.noreply.github.com>
2021-01-25 01:28:17 +0600

    25-1-21

--- Controllers/ShowDataController.cs
+++ Controllers/ShowDataController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using CSE_DEPARTMENT.Models;
 using CSE_DEPARTMENT.ViewModel;
 using System.Linq.Dynamic;
+using PagedList;
 
 namespace CSE_DEPARTMENT.Controllers
 {
@@ -13,31 +14,16 @@ namespace CSE_DEPARTMENT.Controllers
     {
         CSE_DEPARTMENT_DBEntities obj = new CSE_DEPARTMENT_DBEntities();
         // GET: ShowData
-        public ActionResult Multidata(int page = 1, string sort = "Name", string sortdir = "asc", string search = "")
+        public ActionResult Multidata(int page = 0)
         {
 
-            int pageSize = 10;
-            int totalRecord = 0;
-            if (page < 1) page = 1;
-            int skip = (page * pageSize) - pageSize;
-            var data = GetRoles(search, sort, sortdir, skip, pageSize, out totalRecord);
-            ViewBag.TotalRows = totalRecord;
-            ViewBag.search = search;
-            return View(data);
+            return View(obj.AspNetRoles.ToList());
         }
 
-        public ActionResult Multidata2(int page = 1, string sort = "Email", string sortdir = "asc", string search = "")
+        public ActionResult Multidata2()
         {
 
-            int pageSize = 10;
-            int totalRecord = 0;
-            if (page < 1) page = 1;
-            int skip = (page * pageSize) - pageSize;
-            var data = GetUsers(search, sort, sortdir, skip, pageSize, out totalRecord);
-            ViewBag.TotalRows = totalRecord;
-            ViewBag.search = search;
-            return View(data);
-
+            return View(obj.AspNetUsers.ToList());
         }
 
 
@@ -58,49 +44,10 @@ namespace CSE_DEPARTMENT.Controllers
             return RedirectToAction("Multidata", "ShowData");
         }
 
-        public List<AspNetUser> GetUsers(string search, string sort, string sortdir, int skip, int pageSize, out int totalRecord)
-        {
-            using (CSE_DEPARTMENT_DBEntities obj = new CSE_DEPARTMENT_DBEntities())
-            {
-                var v = (from a in obj.AspNetUsers
-                         where
-                                 a.Id.Contains(search) ||
-                                 a.Email.Contains(search) ||
-                                 a.PhoneNumber.Contains(search)
-
-                         select a
-                                );
-                totalRecord = v.Count();
-                v = v.OrderBy(sort + " " + sortdir);
-                if (pageSize > 0)
-                {
-                    v = v.Skip(skip).Take(pageSize);
-                }
-                return v.ToList();
-            }
-        }
-
-        public List<AspNetRole> GetRoles(string search, string sort, string sortdir, int skip, int pageSize, out int totalRecord)
-        {
-            using (CSE_DEPARTMENT_DBEntities obj = new CSE_DEPARTMENT_DBEntities())
-            {
-                var v = (from a in obj.AspNetRoles
-                         where
-                                 a.Id.Contains(search) ||
-                                 a.Name.Contains(search)
 
 
-                         select a
-                                );
-                totalRecord = v.Count();
-                v = v.OrderBy(sort + " " + sortdir);
-                if (pageSize > 0)
-                {
-                    v = v.Skip(skip).Take(pageSize);
-                }
-                return v.ToList();
-            }
-        }
+
+
 
 
     }
